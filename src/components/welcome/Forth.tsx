873a4e749758d64fae89a5040afa76643db92bf5
tao <1985504469@qq.com>
2023-03-27 21:36:58 +0800

    四个欢迎页面

--- src/components/welcome/Forth.tsx
+++ src/components/welcome/Forth.tsx
@@ -1,8 +1,21 @@
-import { defineComponent } from 'vue'
+import { defineComponent } from "vue";
+import s from "./First.module.scss";
+import cloud from "../../assets/icons/cloud.svg";
+import { RouterLink } from "vue-router";
 export const Forth = defineComponent({
   setup: (props, context) => {
     return () => (
-      <div>Forth</div>
-    )
-  }
-})
+      <div class={s.wrapper}>
+        <div class={s.card}>
+          <img class={s.icon} src={cloud} />
+          <h2>云备份<br/>再也不怕数据丢失</h2>
+        </div>
+        <div class={s.actions}>
+          <RouterLink class={s.fake} to="/start">跳过</RouterLink>
+          <RouterLink to="/start">完成</RouterLink>
+          <RouterLink class={s.fake} to="/start">跳过</RouterLink>
+        </div>
+      </div>
+    );
+  },
+});
