0df949731c6cd0930c423795c0b07a3d8a57bf6a
tao <1985504469@qq.com>
2023-03-29 19:10:14 +0800

    完成4张欢迎界面卡片的切换

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -16,10 +16,10 @@ export const routes: RouteRecordRaw[] = [
     component: Welcome,
     children: [
       { path: '', redirect: '/welcome/1', },
-      { path: '1', components: { main: First, footer: FirstActions }, },
-      { path: '2', components: { main: Second, footer: SecondActions }, },
-      { path: '3', components: { main: Third, footer: ThirdActions }, },
-      { path: '4', components: { main: Forth, footer: ForthActions }, },
+      { path: '1', name: "welcome1", components: { main: First, footer: FirstActions }, },
+      { path: '2', name: "welcome2", components: { main: Second, footer: SecondActions }, },
+      { path: '3', name: "welcome3", components: { main: Third, footer: ThirdActions }, },
+      { path: '4', name: "welcome4", components: { main: Forth, footer: ForthActions }, },
     ]
   }
 ]
--- src/views/Welcome.tsx
+++ src/views/Welcome.tsx
@@ -1,34 +1,69 @@
-import { defineComponent, ref, Transition, VNode, watchEffect } from 'vue';
-import { RouteLocationNormalizedLoaded, RouterView } from 'vue-router';
-import s from './Welcome.module.scss'
-import { useSwipe } from '../hooks/useSwipe';
+import { defineComponent, ref, Transition, VNode, watchEffect } from "vue";
+import {
+  RouteLocationNormalizedLoaded,
+  RouterView,
+  useRoute,
+  useRouter,
+} from "vue-router";
+import s from "./Welcome.module.scss";
+import { useSwipe } from "../hooks/useSwipe";
+import { throttle } from "../shared/throttle";
 export const Welcome = defineComponent({
   setup: (props, context) => {
-    const main = ref<HTMLElement>()
-    // const {direction, swiping} = useSwipe(main)
-    // watchEffect(() => {
-    //   console.log(swiping.value, direction.value)
-    // })
-    return () => <div class={s.wrapper}>
-      <header>
-        <svg>
-          <use xlinkHref='#mangosteen'></use>
-        </svg>
-        <h1>山竹记账</h1>
-      </header>
-      <main class={s.main} ref={main}>
-        <RouterView name="main">
-          {({ Component: X, route: R }: { Component: VNode, route: RouteLocationNormalizedLoaded }) =>
-            <Transition enterFromClass={s.slide_fade_enter_from} enterActiveClass={s.slide_fade_enter_active}
-              leaveToClass={s.slide_fade_leave_to} leaveActiveClass={s.slide_fade_leave_active}>
-              {X}
-            </Transition>
-          }
-        </RouterView>
-      </main>
-      <footer>
-        <RouterView name="footer" />
-      </footer>
-    </div>
-  }
-})
+    const main = ref<HTMLElement>();
+    const { direction, swiping } = useSwipe(main, {
+      beforeStart: (e) => e.preventDefault(),
+    });
+    const route = useRoute();
+    const router = useRouter();
+    const push = throttle(() => {
+      if (route.name === "welcome1") {
+        router.push("/welcome/2");
+      } else if (route.name === "welcome2") {
+        router.push("/welcome/3");
+      } else if (route.name === "welcome3") {
+        router.push("/welcome/4");
+      } else if (route.name === "welcome4") {
+        router.push("/start");
+      }
+    }, 500);
+    watchEffect(() => {
+      if (swiping.value && direction.value === 'left') {
+        push()
+      }
+    });
+    return () => (
+      <div class={s.wrapper}>
+        <header>
+          <svg>
+            <use xlinkHref="#mangosteen"></use>
+          </svg>
+          <h1>山竹记账</h1>
+        </header>
+        <main class={s.main} ref={main}>
+          <RouterView name="main">
+            {({
+              Component: X,
+              route: R,
+            }: {
+              Component: VNode;
+              route: RouteLocationNormalizedLoaded;
+            }) => (
+              <Transition
+                enterFromClass={s.slide_fade_enter_from}
+                enterActiveClass={s.slide_fade_enter_active}
+                leaveToClass={s.slide_fade_leave_to}
+                leaveActiveClass={s.slide_fade_leave_active}
+              >
+                {X}
+              </Transition>
+            )}
+          </RouterView>
+        </main>
+        <footer>
+          <RouterView name="footer" />
+        </footer>
+      </div>
+    );
+  },
+});
