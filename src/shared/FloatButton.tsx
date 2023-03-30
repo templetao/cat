b0b34bd75daf56c95ff4307b616838628de26db1
tao <1985504469@qq.com>
2023-03-30 17:37:18 +0800

    创建 FloatButton

--- src/shared/FloatButton.tsx
+++ src/shared/FloatButton.tsx
@@ -1,8 +1,17 @@
-import { defineComponent } from 'vue'
+import { defineComponent, PropType } from 'vue'
+import { Icon } from './Icon'
+import s from './FloatButton.module.scss'
 export const FloatButton = defineComponent({
+  props: {
+    iconName: {
+      type: String as PropType<string>
+    }
+  },
   setup: (props, context) => {
     return () => (
-      <div></div>
+      <div class={s.floatButton}>
+          <Icon name="add" class={s.icon}/>
+      </div>
     )
   }
 })
