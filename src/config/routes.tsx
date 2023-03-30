e8df936d5d2c03007773c9456a3541424c077410
tao <1985504469@qq.com>
2023-03-30 16:55:51 +0800

    创建 StartPage 和 Button

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -7,6 +7,7 @@ import { Second } from "../components/welcome/Second";
 import { SecondActions } from "../components/welcome/SecondActions";
 import { Third } from "../components/welcome/Third";
 import { ThirdActions } from "../components/welcome/ThirdActions";
+import { StartPage } from "../views/StartPage";
 import { Welcome } from "../views/Welcome";
 
 export const routes: RouteRecordRaw[] = [
@@ -21,5 +22,6 @@ export const routes: RouteRecordRaw[] = [
       { path: '3', name: "welcome3", components: { main: Third, footer: ThirdActions }, },
       { path: '4', name: "welcome4", components: { main: Forth, footer: ForthActions }, },
     ]
-  }
+  },
+  { path: '/start', component: StartPage}
 ]
---
+++ src/shared/FloatButton.tsx
@@ -0,0 +1,8 @@
+import { defineComponent } from 'vue'
+export const FloatButton = defineComponent({
+  setup: (props, context) => {
+    return () => (
+      <div></div>
+    )
+  }
+})
