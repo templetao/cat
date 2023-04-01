2a51d1409887158ef26b8a6adf7b8abc37163b7e
tao <1985504469@qq.com>
2023-04-01 16:09:42 +0800

    制作 Center 组件

---
+++ src/shared/Center.tsx
@@ -0,0 +1,23 @@
+import { defineComponent, PropType } from "vue";
+import s from "./Center.module.scss";
+const directionMap = {
+  "-": "horizontal",
+  "horizontal": "horizontal",
+  "|": "vertical",
+  "vertical": "vertical",
+};
+export const Center = defineComponent({
+  props: {
+    direction: {
+      type: String as PropType<"-" | "|" | "horizontal" | "vertical">,
+      default: "horizontal",
+    },
+  },
+  setup: (props, context) => {
+    const extraClass = directionMap[props.direction];
+    return () => 
+      <div class={[s.center, extraClass]}>
+        {context.slots.default?.()}
+      </div>;
+  },
+});
--- src/views/StartPage.tsx
+++ src/views/StartPage.tsx
@@ -1,6 +1,8 @@
 import { defineComponent } from "vue";
 import { Button } from "../shared/Button";
+import { Center } from "../shared/Center";
 import { FloatButton } from "../shared/FloatButton";
+import { Icon } from "../shared/Icon";
 import s from "./StartPage.module.scss";
 export const StartPage = defineComponent({
   setup: (props, context) => {
@@ -9,8 +11,12 @@ export const StartPage = defineComponent({
     }
     return () => (
       <div>
+        <menu>menu</menu>
+        <Center class={s.pig_wrapper}>
+          <Icon name="pig" class={s.pig}/>
+        </Center>
         <div class={s.button_wrapper}>
-          <Button class={s.button} onClick={onClick}>测试</Button>
+          <Button class={s.button} onClick={onClick}>开始记账</Button>
         </div>
         <FloatButton iconName="add"/>
       </div>
