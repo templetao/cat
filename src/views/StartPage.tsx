210f634880c742417993a242fc7362dff2c770f9
tao <1985504469@qq.com>
2023-04-01 18:38:48 +0800

    创建 /items 和 /items/create 路由

--- src/views/StartPage.tsx
+++ src/views/StartPage.tsx
@@ -1,4 +1,5 @@
 import { defineComponent, ref } from "vue";
+import { RouterLink } from "vue-router";
 import { Button } from "../shared/Button";
 import { Center } from "../shared/Center";
 import { FloatButton } from "../shared/FloatButton";
@@ -9,30 +10,34 @@ import s from "./StartPage.module.scss";
 
 export const StartPage = defineComponent({
   setup: (props, context) => {
-    const refOverlayVisible = ref(false)
+    const refOverlayVisible = ref(false);
     const onClickMenu = () => {
-      refOverlayVisible.value = !refOverlayVisible.value
-    }
+      refOverlayVisible.value = !refOverlayVisible.value;
+    };
     return () => (
       <div>
-        <Navbar>{
-          {
-            default: () => '山竹记账',
-            icon: () => <Icon name="menu" class={s.navIcon} onClick={onClickMenu}/>
-          }
-        }</Navbar>
+        <Navbar>
+          {{
+            default: () => "山竹记账",
+            icon: () => (
+              <Icon name="menu" class={s.navIcon} onClick={onClickMenu} />
+            ),
+          }}
+        </Navbar>
         <Center class={s.pig_wrapper}>
           <Icon name="pig" class={s.pig} />
         </Center>
         <div class={s.button_wrapper}>
-          <Button class={s.button}>
-            开始记账
-          </Button>
+          <RouterLink to="/items/create">
+            <Button class={s.button}>开始记账</Button>
+          </RouterLink>
         </div>
-        <FloatButton iconName="add" />
-        {
-          refOverlayVisible.value && <Overlay onClose={() => refOverlayVisible.value = false} />
-        }
+        <RouterLink to="/items/create">
+          <FloatButton iconName="add" />
+        </RouterLink>
+        {refOverlayVisible.value && (
+          <Overlay onClose={() => (refOverlayVisible.value = false)} />
+        )}
       </div>
     );
   },
