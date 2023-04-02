90b1a8a891111d60edd66dbc3f8d7d989418d06c
tao <1985504469@qq.com>
2023-04-02 17:01:01 +0800

    给 date 和 amount 加样式

--- src/components/item/InputPad.tsx
+++ src/components/item/InputPad.tsx
@@ -10,36 +10,36 @@ export const InputPad = defineComponent({
   },
   setup: (props, context) => {
     const buttons = [
-      { text: '1', onClick: () => { } },
-      { text: '2', onClick: () => { } },
-      { text: '3', onClick: () => { } },
-      { text: '清空', onClick: () => { } },
-      { text: '4', onClick: () => { } },
-      { text: '5', onClick: () => { } },
-      { text: '6', onClick: () => { } },
-      { text: '+', onClick: () => { } },
-      { text: '7', onClick: () => { } },
-      { text: '8', onClick: () => { } },
-      { text: '9', onClick: () => { } },
-      { text: '-', onClick: () => { } },
-      { text: '.', onClick: () => { } },
-      { text: '0', onClick: () => { } },
-      { text: '删', onClick: () => { } },
-      { text: '提交', onClick: () => { } },
-    ]
+      { text: "1", onClick: () => {} },
+      { text: "2", onClick: () => {} },
+      { text: "3", onClick: () => {} },
+      { text: "清空", onClick: () => {} },
+      { text: "4", onClick: () => {} },
+      { text: "5", onClick: () => {} },
+      { text: "6", onClick: () => {} },
+      { text: "+", onClick: () => {} },
+      { text: "7", onClick: () => {} },
+      { text: "8", onClick: () => {} },
+      { text: "9", onClick: () => {} },
+      { text: "-", onClick: () => {} },
+      { text: ".", onClick: () => {} },
+      { text: "0", onClick: () => {} },
+      { text: "删", onClick: () => {} },
+      { text: "提交", onClick: () => {} },
+    ];
     return () => (
       <>
-        <div>
-          <span>
-            <Icon name="date" />
+        <div class={s.dateAndAmount}>
+          <span class={s.date}>
+            <Icon name="date" class={s.icon} />
             <span>2022-01-01</span>
           </span>
-          <span class={s.amount}></span>
+          <span class={s.amount}>199.12</span>
         </div>
         <div class={s.buttons}>
-          {buttons.map(button =>
-            <button onClick={button.onClick}>{button.text}</button> 
-          )}
+          {buttons.map((button) => (
+            <button onClick={button.onClick}>{button.text}</button>
+          ))}
         </div>
       </>
     );
