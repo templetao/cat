fed1b42d437613bfa451f48aec8ee80d26063553
tao <1985504469@qq.com>
2023-04-02 17:45:27 +0800

    修复日期选择 bug

--- src/components/item/InputPad.tsx
+++ src/components/item/InputPad.tsx
@@ -1,55 +1,73 @@
-import { defineComponent, PropType, ref } from 'vue';
-import { Icon } from '../../shared/Icon';
-import { time } from '../../shared/time';
-import s from './InputPad.module.scss';
-import { DatetimePicker, Popup } from 'vant';
+import { defineComponent, PropType, ref } from "vue";
+import { Icon } from "../../shared/Icon";
+import { time } from "../../shared/time";
+import s from "./InputPad.module.scss";
+import { DatetimePicker, Popup } from "vant";
 export const InputPad = defineComponent({
   props: {
     name: {
-      type: String as PropType<string>
-    }
+      type: String as PropType<string>,
+    },
   },
   setup: (props, context) => {
-    const now = new Date()
-    const refDate = ref<Date>(now)
+    const now = new Date();
+    const refDate = ref<Date>(now);
     const buttons = [
-      { text: '1', onClick: () => { } },
-      { text: '2', onClick: () => { } },
-      { text: '3', onClick: () => { } },
-      { text: '清空', onClick: () => { } },
-      { text: '4', onClick: () => { } },
-      { text: '5', onClick: () => { } },
-      { text: '6', onClick: () => { } },
-      { text: '+', onClick: () => { } },
-      { text: '7', onClick: () => { } },
-      { text: '8', onClick: () => { } },
-      { text: '9', onClick: () => { } },
-      { text: '-', onClick: () => { } },
-      { text: '.', onClick: () => { } },
-      { text: '0', onClick: () => { } },
-      { text: '删', onClick: () => { } },
-      { text: '提交', onClick: () => { } },
-    ]
-    const refShowPop = ref(false)
-    return () => <>
-      <div class={s.dateAndAmount}>
-        <span class={s.date}>
-          <Icon name="date" class={s.icon} />
-          <span>
-            <span onClick={() => refShowPop.value = true}>{time(refDate.value).format()}</span>
-            <Popup position='bottom' v-model:show={refShowPop.value}>
-              <DatetimePicker v-model={refDate.value} type="date" title="选择年月日"
-                onConfirm={() => refShowPop.value = false} />
-            </Popup>
+      { text: "1", onClick: () => {} },
+      { text: "2", onClick: () => {} },
+      { text: "3", onClick: () => {} },
+      { text: "清空", onClick: () => {} },
+      { text: "4", onClick: () => {} },
+      { text: "5", onClick: () => {} },
+      { text: "6", onClick: () => {} },
+      { text: "+", onClick: () => {} },
+      { text: "7", onClick: () => {} },
+      { text: "8", onClick: () => {} },
+      { text: "9", onClick: () => {} },
+      { text: "-", onClick: () => {} },
+      { text: ".", onClick: () => {} },
+      { text: "0", onClick: () => {} },
+      { text: "删", onClick: () => {} },
+      { text: "提交", onClick: () => {} },
+    ];
+    const refDatePickerVisible = ref(false);
+    const showDatePicker = () => (refDatePickerVisible.value = true);
+    const hideDatePicker = () => (refDatePickerVisible.value = false);
+    const setDate = (date: Date) => {
+      refDate.value = date;
+      hideDatePicker();
+    };
+    return () => (
+      <>
+        <div class={s.dateAndAmount}>
+          <span class={s.date}>
+            <Icon name="date" class={s.icon} />
+            <span>
+              <span onClick={showDatePicker}>
+                {time(refDate.value).format()}
+              </span>
+              <Popup
+                position="bottom"
+                v-model:show={refDatePickerVisible.value}
+              >
+                <DatetimePicker
+                  value={refDate.value}
+                  type="date"
+                  title="选择年月日"
+                  onConfirm={setDate}
+                  onCancel={hideDatePicker}
+                />
+              </Popup>
+            </span>
           </span>
-        </span>
-        <span class={s.amount}>199.12</span>
-      </div>
-      <div class={s.buttons}>
-        {buttons.map(button =>
-          <button onClick={button.onClick}>{button.text}</button>
-        )}
-      </div>
-    </>
-  }
-})
+          <span class={s.amount}>199.12</span>
+        </div>
+        <div class={s.buttons}>
+          {buttons.map((button) => (
+            <button onClick={button.onClick}>{button.text}</button>
+          ))}
+        </div>
+      </>
+    );
+  },
+});
