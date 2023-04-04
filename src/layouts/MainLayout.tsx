73fd093468c3242530809dac6ecd88b815498f57
tao <1985504469@qq.com>
2023-04-04 16:50:20 +0800

    完成记一笔的样式

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,17 +1,18 @@
-import { defineComponent, PropType } from 'vue';
-import { Navbar } from '../shared/Navbar';
+import { defineComponent, PropType } from "vue";
+import { Navbar } from "../shared/Navbar";
+import s from './MainLayout.module.scss'
 export const MainLayout = defineComponent({
   setup: (props, context) => {
     return () => (
       <div>
-      <Navbar>{
-        {
-          default: () => context.slots.title?.(),
-          icon: () => context.slots.icon?.(),
-        }
-      }</Navbar>
-      {context.slots.default?.()}
+        <Navbar>
+          {{
+            default: () => context.slots.title?.(),
+            icon: () => context.slots.icon?.(),
+          }}
+        </Navbar>
+        {context.slots.default?.()}
       </div>
-    )
-  }
-})
+    );
+  },
+});
