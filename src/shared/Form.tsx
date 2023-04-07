cc2cf30e85aad166c87ca1574d9cfd357e93fa9b
tao <1985504469@qq.com>
2023-04-07 21:05:24 +0800

    FormItem 可以响应  click 了

--- src/shared/Form.tsx
+++ src/shared/Form.tsx
@@ -35,7 +35,8 @@ export const FormItem = defineComponent({
       type: String
     },
     placeholder: String,
-    options: Array as PropType<Array<{value: string, text: string}>>
+    options: Array as PropType<Array<{value: string, text: string}>>,
+    onClick: Function as PropType<() => void>
   },
   setup: (props, context) => {
     const refDateVisible = ref(false)
@@ -56,7 +57,7 @@ export const FormItem = defineComponent({
           return <>
             <input class={[s.formItem, s.input, s.validationCodeInput]}
               placeholder={props.placeholder} />
-            <Button class={[s.formItem, s.button, s.validationCodeButton]}>
+            <Button onClick={props.onClick} class={[s.formItem, s.button, s.validationCodeButton]}>
               发送验证码
             </Button>
           </>
--- src/views/SignInPage.tsx
+++ src/views/SignInPage.tsx
@@ -26,6 +26,9 @@ export const SignInPage = defineComponent({
         { key: 'code', type: 'required', message: '必填'},
       ]))
     }
+    const onClickSendValidationCode = () => {
+      console.log('111')
+    }
     return () => (
       <MainLayout>{{
         title: () => '登录',
@@ -42,6 +45,7 @@ export const SignInPage = defineComponent({
                 v-model={formData.email} error={errors.email?.[0]} />
               <FormItem label='验证码' type='validationCode'
                 placeholder='请输入六位数字'
+                onClick={onClickSendValidationCode}
                 v-model={formData.code} error={errors.code?.[0]} />
               <FormItem style={{ paddingTop: '96px'}}>
                 <Button>登录</Button>
