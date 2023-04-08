a3c4355c89563cd177f2b8db21485ec387cc9ce9
tao <1985504469@qq.com>
2023-04-08 12:36:57 +0800

    验证码 倒计时 60s

--- src/shared/Form.tsx
+++ src/shared/Form.tsx
@@ -1,3 +1,4 @@
+import { time } from 'echarts';
 import { DatetimePicker, Popup } from 'vant';
 import { computed, defineComponent, PropType, ref, VNode } from 'vue';
 import { Button } from './Button';
@@ -36,10 +37,28 @@ export const FormItem = defineComponent({
     },
     placeholder: String,
     options: Array as PropType<Array<{value: string, text: string}>>,
-    onClick: Function as PropType<() => void>
+    onClick: Function as PropType<() => void>,
+    countFrom: {
+      type: Number,
+      default: 60
+    }
   },
   setup: (props, context) => {
     const refDateVisible = ref(false)
+    const timer = ref<number>()
+    const count = ref<number>(props.countFrom)
+    const isCounting = computed(() => !!timer.value)
+    const onClickSendValidationCode = () => {
+      props.onClick?.()
+      timer.value = setInterval(() => {
+        count.value -= 1
+        if (count.value === 0) {
+          clearInterval(timer.value)
+          timer.value = undefined
+          count.value = props.countFrom
+        }
+      }, 1000)
+    }
     const content = computed(() => {
       switch (props.type) {
         case 'text':
@@ -57,8 +76,8 @@ export const FormItem = defineComponent({
           return <>
             <input class={[s.formItem, s.input, s.validationCodeInput]}
               placeholder={props.placeholder} />
-            <Button onClick={props.onClick} class={[s.formItem, s.button, s.validationCodeButton]}>
-              发送验证码
+            <Button disabled={isCounting.value} onClick={onClickSendValidationCode} class={[s.formItem, s.button, s.validationCodeButton]}>
+              {isCounting.value ? `${count.value}后可重新发送` : '发送验证码'}
             </Button>
           </>
         case 'select':
