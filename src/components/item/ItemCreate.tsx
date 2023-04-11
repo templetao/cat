2872fe1f8bc6dce2aaca9cd0c29edec2b4ea1cca
tao <1985504469@qq.com>
2023-04-11 16:26:06 +0800

    封装tags组件

--- src/components/item/ItemCreate.tsx
+++ src/components/item/ItemCreate.tsx
@@ -1,12 +1,10 @@
-import { defineComponent, onMounted, PropType, ref } from 'vue';
+import { defineComponent, PropType, ref } from 'vue';
 import { MainLayout } from '../../layouts/MainLayout';
-import { Button } from '../../shared/Button';
-import { http } from '../../shared/Http';
 import { Icon } from '../../shared/Icon';
 import { Tabs, Tab } from '../../shared/Tabs';
-import { useTags } from '../../shared/useTags';
 import { InputPad } from './InputPad';
 import s from './ItemCreate.module.scss';
+import { Tags } from './Tags';
 export const ItemCreate = defineComponent({
   props: {
     name: {
@@ -15,31 +13,6 @@ export const ItemCreate = defineComponent({
   },
   setup: (props, context) => {
     const refKind = ref('支出')
-    const { hasMore, tags: expensesTags, fetchTags } = useTags((page) => {
-      return http.get<Resources<Tag>>('/tags', {
-        kind: 'expenses',
-        page: page + 1,
-        _mock: 'tagIndex'
-      })
-    })
-
-    const { hasMore: hasMore2, tags: incomeTags, fetchTags: fetchTags2 } =
-      useTags((page) => {
-        return http.get<Resources<Tag>>('/tags', {
-          kind: 'income',
-          page: page + 1,
-          _mock: 'tagIndex'
-        })
-      })
-
-    // onMounted(async () => {
-    //   const response = await http.get<{ resources: Tag[] }>('/tags', {
-    //     kind: 'income',
-    //     _mock: 'tagIndex'
-    //   })
-    //   refIncomeTags.value = response.data.resources
-    // })
-    // const refIncomeTags = ref<Tag[]>([])
     return () => (
       <MainLayout class={s.layout}>{{
         title: () => '记一笔',
@@ -48,60 +21,10 @@ export const ItemCreate = defineComponent({
           <div class={s.wrapper}>
             <Tabs v-model:selected={refKind.value} class={s.tabs}>
               <Tab name="支出" >
-                <div class={s.tags_wrapper}>
-                  <div class={s.tag}>
-                    <div class={s.sign}>
-                      <Icon name="add" class={s.createTag} />
-                    </div>
-                    <div class={s.name}>
-                      新增
-                    </div>
-                  </div>
-                  {expensesTags.value.map(tag =>
-                    <div class={[s.tag, s.selected]}>
-                      <div class={s.sign}>
-                        {tag.sign}
-                      </div>
-                      <div class={s.name}>
-                        {tag.name}
-                      </div>
-                    </div>
-                  )}
-                </div>
-                <div class={s.more}>
-                  {hasMore.value ?
-                    <Button class={s.loadMore} onClick={fetchTags}>加载更多</Button> :
-                    <span class={s.noMore}>没有更多</span>
-                  }
-                </div>
+                <Tags kind='expenses' />
               </Tab>
               <Tab name="收入" >
-                <div class={s.tags_wrapper}>
-                  <div class={s.tag}>
-                    <div class={s.sign}>
-                      <Icon name="add" class={s.createTag} />
-                    </div>
-                    <div class={s.name}>
-                      新增
-                    </div>
-                  </div>
-                  {incomeTags.value.map(tag =>
-                    <div class={[s.tag, s.selected]}>
-                      <div class={s.sign}>
-                        {tag.sign}
-                      </div>
-                      <div class={s.name}>
-                        {tag.name}
-                      </div>
-                    </div>
-                  )}
-                </div>
-                <div class={s.more}>
-                  {hasMore2.value ?
-                    <Button class={s.loadMore} onClick={fetchTags2}>加载更多</Button> :
-                    <span class={s.noMore}>没有更多</span>
-                  }
-                </div>
+                <Tags kind='income' />
               </Tab>
             </Tabs>
             <div class={s.inputPad_wrapper}>
