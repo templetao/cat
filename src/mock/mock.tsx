51451a9426dc34cb7aadf60f6c84edd6b842bbcc
tao <1985504469@qq.com>
2023-04-11 16:04:48 +0800

    封装 useTags

--- src/mock/mock.tsx
+++ src/mock/mock.tsx
@@ -35,11 +35,13 @@ export const mockTagIndex: Mock = (config) => {
     resources: createTag(n), pager: createPage(page)
   })
 
-  if (kind === 'expenses' && (page === 1 || !page)) {
+  if (kind === 'expenses' && (!page || page === 1)) {
     return [200, createBody(25)]
   } else if (kind === 'expenses' && page === 2) {
     return [200, createBody(1)]
+  } else if (kind === 'income' && (!page || page === 1)) {
+    return [200, createBody(25)]
   } else {
-    return [200, { resources: createTag(25) }]
+    return [200, createBody(1)]
   }
 }
