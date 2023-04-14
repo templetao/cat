c3d680993df7d9cce90d6ad5bbbfaf342632fac8
tao <1985504469@qq.com>
2023-04-14 21:05:34 +0800

    切换 kind 时重新请求数据

--- src/components/statistics/Charts.tsx
+++ src/components/statistics/Charts.tsx
@@ -1,4 +1,4 @@
-import { computed, defineComponent, onMounted, PropType, ref } from 'vue';
+import { computed, defineComponent, onMounted, PropType, ref, watch } from 'vue';
 import { FormItem } from '../../shared/Form';
 import s from './Charts.module.scss';
 import { LineChart } from './LineChart';
@@ -40,7 +40,7 @@ export const Charts = defineComponent({
         return [new Date(time).toISOString(), amount]
       })
     })
-    onMounted(async () => {
+    const fetchData1 = async () => {
       const response = await http.get<{ groups: Data1; summary: number }>('/items/summary', {
         happen_after: props.startDate,
         happen_before: props.endDate,
@@ -49,9 +49,10 @@ export const Charts = defineComponent({
         _mock: 'itemSummary'
       })
       data1.value = response.data.groups
-    })
+    }
 
-    //data2
+    onMounted(fetchData1)
+    watch(() => kind.value, fetchData1)
 
     const data2 = ref<Data2>([])
     const betterData2 = computed<{ name: string; value: number }[]>(() =>
@@ -60,8 +61,8 @@ export const Charts = defineComponent({
         value: item.amount
       }))
     )
-    
-    const betterData3 = computed<{tag:Tag, amount:number, percent: number}[]>(()=>{
+
+    const betterData3 = computed<{ tag: Tag, amount: number, percent: number }[]>(() => {
       const total = data2.value.reduce((sum, item) => sum + item.amount, 0)
       return data2.value.map(item => ({
         ...item,
@@ -69,7 +70,7 @@ export const Charts = defineComponent({
       }))
     })
 
-    onMounted(async () => {
+    const fetchData2 = async () => {
       const response = await http.get<{ groups: Data2; summary: number }>('/items/summary', {
         happen_after: props.startDate,
         happen_before: props.endDate,
@@ -78,7 +79,9 @@ export const Charts = defineComponent({
         _mock: 'itemSummary'
       })
       data2.value = response.data.groups
-    })
+    }
+    onMounted(fetchData2)
+    watch(() => kind.value, fetchData2)
     return () => (
       <div class={s.wrapper}>
         <FormItem label='类型' type="select" options={[
@@ -87,7 +90,7 @@ export const Charts = defineComponent({
         ]} v-model={kind.value} />
         <LineChart data={betterData1.value} />
         <PieChart data={betterData2.value} />
-        <Bars data={betterData3.value}/>
+        <Bars data={betterData3.value} />
       </div>
     )
   }
--- src/mock/mock.tsx
+++ src/mock/mock.tsx
@@ -6,7 +6,8 @@ type Mock = (config: AxiosRequestConfig) => [number, any]
 faker.setLocale('zh_CN');
 
 export const mockItemSummary: Mock = config => {
-  if (config.params.group_by === 'happen_at') {
+  const { group_by, kind } = config.params
+  if (group_by === 'happen_at' && kind === 'expenses') {
     return [200, {
       groups: [
         { "happen_at": "2023-04-18T00:00:00.000+0800", "amount": 100 },
@@ -15,10 +16,28 @@ export const mockItemSummary: Mock = config => {
       ],
       summary: 600
     }]
-  } else {
+  } else if (group_by === 'happen_at' && kind === 'income') {
+    return [200, {
+      groups: [
+        { happen_at: '2022-07-08T00:00:00.000+0800', amount: 100 },
+        { happen_at: '2022-07-12T00:00:00.000+0800', amount: 300 },
+        { happen_at: '2022-07-19T00:00:00.000+0800', amount: 200 }
+      ],
+      summary: 600
+    }]
+  } else if (group_by === 'tag_id' && kind === 'expenses') {
     return [200, {
       groups: [
         { tag_id: 1, tag: { id: 1, name: '交通', sign: faker.internet.emoji() }, amount: 100 },
+        { tag_id: 2, tag: { id: 2, name: '吃饭', sign: faker.internet.emoji() }, amount: 300 },
+        { tag_id: 3, tag: { id: 3, name: '购物', sign: faker.internet.emoji() }, amount: 200 }
+      ],
+      summary: 900
+    }]
+  } else {
+    return [200, {
+      groups: [
+        { tag_id: 1, tag: { id: 1, name: '交通', sign: faker.internet.emoji() }, amount: 400 },
         { tag_id: 2, tag: { id: 2, name: '吃饭', sign: faker.internet.emoji() }, amount: 300 },
         { tag_id: 3, tag: { id: 3, name: '购物', sign: faker.internet.emoji() }, amount: 200 }
       ],
