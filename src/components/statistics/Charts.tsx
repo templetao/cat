bdc2e8831a2e976af0680d1a09d1ad934b62af5a
tao <1985504469@qq.com>
2023-04-14 20:02:17 +0800

    完成饼图

--- src/components/statistics/Charts.tsx
+++ src/components/statistics/Charts.tsx
@@ -11,7 +11,8 @@ const DAY = 24 * 3600 * 1000
 
 type Data1Item = { happen_at: string, amount: number }
 type Data1 = Data1Item[]
-
+type Data2Item = { tag_id: number; tag: Tag; amount: number }
+type Data2 = Data2Item[]
 export const Charts = defineComponent({
   props: {
     startDate: {
@@ -26,27 +27,49 @@ export const Charts = defineComponent({
   setup: (props, context) => {
     const kind = ref('expenses')
     const data1 = ref<Data1>([])
-    const betterData1 = computed(() => {
+    const betterData1 = computed<[string, number][]>(() => {
       if (!props.startDate || !props.endDate) { return [] }
       const diff = new Date(props.endDate).getTime() - new Date(props.startDate).getTime()
       const n = diff / DAY + 1
       return Array.from({ length: n }).map((_, i) => {
         const time = new Time(props.startDate + 'T00:00:00.000+0800').add(i, 'day').getTimestamp()
         const item = data1.value[0]
-        const amount = (item && new Date(item.happen_at).getTime() === time)
+        const amount = item && new Date(item.happen_at).getTime() === time
           ? data1.value.shift()!.amount
           : 0
         return [new Date(time).toISOString(), amount]
       })
     })
     onMounted(async () => {
-      const response = await http.get<{ groups: Data1, summary: number }>('/items/summary', {
+      const response = await http.get<{ groups: Data1; summary: number }>('/items/summary', {
         happen_after: props.startDate,
         happen_before: props.endDate,
         kind: kind.value,
+        group_by: 'happen_at',
         _mock: 'itemSummary'
       })
       data1.value = response.data.groups
+    })
+
+    //data2
+
+    const data2 = ref<Data2>([])
+    const betterData2 = computed<{ name: string; value: number }[]>(() =>
+      data2.value.map((item) => ({
+        name: item.tag.name,
+        value: item.amount
+      }))
+    )
+
+    onMounted(async () => {
+      const response = await http.get<{ groups: Data2; summary: number }>('/items/summary', {
+        happen_after: props.startDate,
+        happen_before: props.endDate,
+        kind: kind.value,
+        group_by: 'tag_id',
+        _mock: 'itemSummary'
+      })
+      data2.value = response.data.groups
     })
     return () => (
       <div class={s.wrapper}>
@@ -55,7 +78,7 @@ export const Charts = defineComponent({
           { value: 'income', text: '收入' }
         ]} v-model={kind.value} />
         <LineChart data={betterData1.value} />
-        <PieChart />
+        <PieChart data={betterData2.value} />
         <Bars />
       </div>
     )
