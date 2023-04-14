9058b809d2796952ecdc641446b2945a7bd356ae
tao <1985504469@qq.com>
2023-04-14 20:57:46 +0800

    完成 bar chart

--- src/components/statistics/Charts.tsx
+++ src/components/statistics/Charts.tsx
@@ -60,6 +60,14 @@ export const Charts = defineComponent({
         value: item.amount
       }))
     )
+    
+    const betterData3 = computed<{tag:Tag, amount:number, percent: number}[]>(()=>{
+      const total = data2.value.reduce((sum, item) => sum + item.amount, 0)
+      return data2.value.map(item => ({
+        ...item,
+        percent: Math.round(item.amount / total * 100)
+      }))
+    })
 
     onMounted(async () => {
       const response = await http.get<{ groups: Data2; summary: number }>('/items/summary', {
@@ -79,7 +87,7 @@ export const Charts = defineComponent({
         ]} v-model={kind.value} />
         <LineChart data={betterData1.value} />
         <PieChart data={betterData2.value} />
-        <Bars />
+        <Bars data={betterData3.value}/>
       </div>
     )
   }
