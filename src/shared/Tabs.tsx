f5c778839feba59c489dd2f24a87168811be2a19
tao <1985504469@qq.com>
2023-04-14 19:04:21 +0800

    每次切换tab时重新渲染图表

--- src/shared/Tabs.tsx
+++ src/shared/Tabs.tsx
@@ -9,6 +9,10 @@ export const Tabs = defineComponent({
       type: String as PropType<string>,
       required: false,
     },
+    rerenderOnSelect: {
+      type: Boolean as PropType<boolean>,
+      default: false
+    }
   },
   emits: ['update:selected'],
   setup: (props, context) => {
@@ -33,11 +37,16 @@ export const Tabs = defineComponent({
               {item.props?.name}
             </li>)}
         </ol>
-        <div>
-          {tabs.map(item =>
-            <div v-show={item.props?.name === props.selected}>{item}</div>
-          )}
-        </div>
+        {props.rerenderOnSelect ?
+          <div>
+            {tabs.find(item => item.props?.name === props.selected)}
+          </div> :
+          <div>
+            {tabs.map(item =>
+              <div v-show={item.props?.name === props.selected}>{item}</div>
+            )}
+          </div>
+        }
       </div>
     }
   }
