35295bcdbbf381925d0b36e3c4a5c95aa644d155
tao <1985504469@qq.com>
2023-04-15 19:46:41 +0800

    将 tags_id 改为 tag_ids

--- src/components/item/ItemCreate.tsx
+++ src/components/item/ItemCreate.tsx
@@ -1,14 +1,12 @@
 import { AxiosError } from 'axios';
 import { Dialog } from 'vant';
-import { defineComponent, onMounted, PropType, reactive, ref } from 'vue';
+import { defineComponent, PropType, reactive } from 'vue';
 import { useRouter } from 'vue-router';
 import { MainLayout } from '../../layouts/MainLayout';
 import { BackIcon } from '../../shared/BackIcon';
-import { Button } from '../../shared/Button';
 import { http } from '../../shared/Http';
-import { Icon } from '../../shared/Icon';
 import { Tabs, Tab } from '../../shared/Tabs';
-import { useTags } from '../../shared/useTags';
+import { hasError, validate } from '../../shared/validate';
 import { InputPad } from './InputPad';
 import s from './ItemCreate.module.scss';
 import { Tags } from './Tags';
@@ -19,12 +17,18 @@ export const ItemCreate = defineComponent({
     },
   },
   setup: (props, context) => {
-    const formData = reactive({
-      kind: '支出',
-      tags_id: [],
+    const formData = reactive<Partial<Item>>({
+      kind: 'expenses',
+      tag_ids: [],
       amount: 0,
       happen_at: new Date().toISOString(),
     });
+    const errors = reactive<{ [k in keyof typeof formData]?: string[] }>({
+      kind: [],
+      tag_ids: [],
+      amount: [],
+      happen_at: [],
+    })
     const router = useRouter();
     const onError = (error: AxiosError<ResourceError>) => {
       if (error.response?.status === 422) {
@@ -36,6 +40,20 @@ export const ItemCreate = defineComponent({
       throw error;
     };
     const onSubmit = async () => {
+      Object.assign(errors, { kind: [], tag_ids: [], amount: [], happen_at: [], })
+      Object.assign(errors, validate(formData, [
+        { key: 'kind', type: 'required', message: '类型必填' },
+        { key: 'tag_ids', type: 'required', message: '标签必填' },
+        { key: 'amount', type: 'required', message: '金额必填' },
+        { key: 'amount', type: 'notEqual', value: 0, message: '金额不能为零' },
+        { key: 'happen_at', type: 'required', message: '时间必填' },
+      ]))
+      if (hasError(errors)) {
+        Dialog.alert({
+          title: '出错',
+          message: Object.values(errors).filter(i => i.length > 0).join('\n'),
+        });
+      }
       await http
         .post<Resource<Item>>('/items', formData, {
           _mock: 'itemCreate', _autoLoading: true
@@ -52,16 +70,16 @@ export const ItemCreate = defineComponent({
             <>
               <div class={s.wrapper}>
                 <Tabs v-model:selected={formData.kind} class={s.tabs}>
-                  <Tab name="支出">
+                  <Tab value='expenses' name="支出">
                     <Tags
                       kind="expenses"
-                      v-model:selected={formData.tags_id[0]}
+                      v-model:selected={formData.tag_ids![0]}
                     />
                   </Tab>
-                  <Tab name="收入">
+                  <Tab value='income' name="收入">
                     <Tags
                       kind="income"
-                      v-model:selected={formData.tags_id[0]}
+                      v-model:selected={formData.tag_ids![0]}
                     />
                   </Tab>
                 </Tabs>
--- src/components/tag/TagForm.tsx
+++ src/components/tag/TagForm.tsx
@@ -1,7 +1,6 @@
-import { defineComponent, onMounted, PropType, reactive, toRaw } from 'vue';
+import { defineComponent, onMounted, reactive } from 'vue';
 import { useRoute, useRouter } from 'vue-router';
 import { Button } from '../../shared/Button';
-import { EmojiSelect } from '../../shared/EmojiSelect';
 import { Form, FormItem } from '../../shared/Form';
 import { http } from '../../shared/Http';
 import { onFormError } from '../../shared/onFormError';
@@ -17,7 +16,7 @@ export const TagForm = defineComponent({
       id: undefined,
       name: '',
       sign: '',
-      kind: route.query.kind!.toString(),
+      kind: route.query.kind!.toString() as ('expenses' | 'income'),
     })
     const errors = reactive<{ [k in keyof typeof formData]?: string[] }>({})
     const router = useRouter()
